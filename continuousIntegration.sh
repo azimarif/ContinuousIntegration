5b0db8149f84c512fa3904f8f1173dc448320370
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-26 00:27:41 +0530

    Rename watchTime to timeInterval

--- continuousIntegration.sh
+++ continuousIntegration.sh
@@ -1,18 +1,18 @@
 repoURL=$1
-watchTime=$2
+timeInterval=$2
 repoName=$(echo $repoURL | rev | cut -d '/' -f1 | rev | cut -d '.' -f1)
 
 if [ ! -d "$repoName" ]; then
   git clone "$repoURL"
 fi
 
-if [ -z "$watchTime" ]; then
-  watchTime=60
+if [ -z "$timeInterval" ]; then
+  timeInterval=60
 fi
 
 cd "$repoName"
 
 while true; do
   git pull; clear; date ; tree; mocha --reporter landing
-  sleep "$watchTime"
+  sleep "$timeInterval"
 done
